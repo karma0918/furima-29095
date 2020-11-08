3d50986de6d517d22d2d23e87e1154870d6ba3f6
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-11-08 18:08:19 +0900

    Revert "Revert "rspecでテストコード""
    This reverts commit 2f4fee442bf27ebdbe7ccb5c033e59e769982471.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-  #validates :password, presence: true, length: { maximum: 6 }
-  #validates :email , presence: true
+  validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+  validates :password, presence: true, length: { maximum: 6 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
