f01f6d4f36320a8987b76ece9e4a95bbe786d0b2
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-11-08 18:02:45 +0900

    Revert "nicknameの追加"
    This reverts commit ecc92bd2d9fd43116cc23031bac861e7e551bb74.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  #validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-  #validates :password, presence: true, length: { maximum: 6 }
-  #validates :email , presence: true
 end
