17cab0a2a23e2cce225b94594b97b70bd56893f4
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-11-08 22:21:19 +0900

    viewの追加、修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: 'is invalid. Input half-width characters.' }
+    validates :password, presence: true, length: { maximum: 6 }, format: {with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数で入力してください。"}
+  end
+  end
 
-  validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: 'is invalid. Input half-width characters.' }
-  validates :password, presence: true, length: { maximum: 6 }
-  validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
-end
