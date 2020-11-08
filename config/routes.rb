10ac8f0546449e525e09798825f2f1a6178753bc
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-11-08 19:09:17 +0900

    コンフリクト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   devise_for :users
   root to: 'items#index'
-=======
-  get 'orders', to: 'orders#index'
->>>>>>> parent of ff5b1c0... db設計
+
 end
