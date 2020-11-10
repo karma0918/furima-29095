7078581635fe7f62c206ea0f94031e9bb32ad4bb
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-11-10 21:17:25 +0900

    バリデーションとエラーメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: 'is invalid. Input half-width characters.' }
     validates :password, presence: true, length: { maximum: 6 }, format: {with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数で入力してください。"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください'}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力してください'}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ'}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ'}
   end
   end
 
--- db/migrate/20201106073959_devise_create_users.rb
+++ db/migrate/20201106073959_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name, null: false, default: ""
       t.string :first_name_kana, null: false, default: ""
       t.string :last_name_kana, null: false, default: ""
-      t.datetime :birthday, null: false, default: ""
+      t.datetime :birthday
 
 
       ## Recoverable
