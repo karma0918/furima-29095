4c24c2bf745f5d7c62f4111d66c0e4e136f71e5f
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-11-22 15:57:31 +0900

    javascriptの実装

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,4 +1,12 @@
-window.addEventListener('load', function(){
+console.log("ok")
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const Profit = document.getElementById("profit");
+  priceInput.addEventListener( "input", ()=>{
+    const inputValue = priceInput.value;
+    tax.innerHTML = Math.ceil(inputValue*0.1);
+    Profit.innerHTML = Math.floor(inputValue*0.9);
 
-  const tax = document.getElementById("add-tax-price")
-})
+  });
+});
