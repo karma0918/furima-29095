bc40cfe9717c55cfd7df67de67d2468949efc894
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-12-05 15:40:36 +0900

    エラーメッセージの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new,:create]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit,:update]
 
   def index
     @items = Item.includes(:user).order("created_at ASC")
@@ -25,11 +25,11 @@ class ItemsController < ApplicationController
 
  end
  def update
-  item = Item.find(params[:id])
-  if item.update(item_params)
-    redirect_to root_path
-  else
-    render :edit
+
+  if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
   end
  end
 # def destroy
@@ -39,7 +39,6 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit( :name, :price, :description, :category_id, :condition_id, :cost_id, :area_id, :way_time_id, :image).merge(user_id: current_user.id)
-
   end
   def set_item
     @item = Item.find(params[:id])
