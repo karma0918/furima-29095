77be12f4c84c862bbbfc3f83794ad8696ca4c501
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-12-20 22:40:25 +0900

    sessionsの最適化

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
+
+  before_action :set_item, only: [:create,:index]
+
   def index
 
     unless user_signed_in?
       redirect_to new_user_registration_path
     end
-    @item = Item.find(params[:item_id])
+
     @order_address = OrderAddress.new
     if  current_user == @item.user
       redirect_to root_path
@@ -16,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
    def create
-    @item = Item.find(params[:item_id])
+
      @order_address = OrderAddress.new(address_params)
      if @order_address.valid?
        @order_address.save
@@ -32,5 +35,8 @@ class SessionsController < ApplicationController
   def address_params
     
     params.require(:order_address).permit(:postal,:area,:city,:address,:building,:phone).merge(item_id: @item[:id],user_id: current_user.id,token: params[:token])
+  end
+  def set_item 
+    @item = Item.find(params[:item_id])
   end
 end
